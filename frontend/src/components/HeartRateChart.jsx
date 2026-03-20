214717acf5b1d280d2ff31edfb00928bea84de50
Aadya2901 <aadyapatel2901@gmail.com>
2026-03-20 17:21:39 +0530

    feat: make charts dynamic for hr, spo2, temperature

--- frontend/src/components/HeartRateChart.jsx
+++ frontend/src/components/HeartRateChart.jsx
@@ -8,16 +8,15 @@ import {
 
 export default function HeartRateChart({ data, dataKey, color }) {
   return (
-    <LineChart width={300} height={200} data={data}>
-      <XAxis hide />
-      <YAxis hide />
+    <LineChart width={350} height={220} data={data}>
+      <XAxis dataKey="time" stroke="#9ca3af" />
+      <YAxis stroke="#9ca3af" />
       <Tooltip />
-
       <Line
         type="monotone"
         dataKey={dataKey}
         stroke={color}
-        strokeWidth={2}
+        strokeWidth={3}
         dot={false}
       />
     </LineChart>
