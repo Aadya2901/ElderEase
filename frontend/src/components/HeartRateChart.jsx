2e01b7727b21c42974f760be11bdf22b255c4f38
Aadya2901 <aadyapatel2901@gmail.com>
2026-03-20 17:33:39 +0530

    fix: use responsive container for charts

--- frontend/src/components/HeartRateChart.jsx
+++ frontend/src/components/HeartRateChart.jsx
@@ -5,20 +5,24 @@ import {
   YAxis,
   Tooltip
 } from "recharts";
+import { ResponsiveContainer } from "recharts";
 
 export default function HeartRateChart({ data, dataKey, color }) {
   return (
-    <LineChart width={350} height={220} data={data}>
-      <XAxis dataKey="time" stroke="#9ca3af" />
-      <YAxis stroke="#9ca3af" />
-      <Tooltip />
-      <Line
-        type="monotone"
-        dataKey={dataKey}
-        stroke={color}
-        strokeWidth={3}
-        dot={false}
-      />
-    </LineChart>
+    <ResponsiveContainer width="100%" height={200}>
+      <LineChart data={data}>
+        <XAxis dataKey="time" stroke="#9ca3af" />
+        <YAxis stroke="#9ca3af" />
+        <Tooltip />
+
+        <Line
+          type="monotone"
+          dataKey={dataKey}
+          stroke={color}
+          strokeWidth={3}
+          dot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
